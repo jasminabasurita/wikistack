fc2b602be82dcca8ba9bebdce9d67d6fbcc996f0
Ruth Hill <ruthhill.e@gmail.com>
2017-11-10 17:05:33 -0500

    initialization

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const pg = require('pg');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+const routes = require('./routes')
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use(routes);
+
+app.listen(3000, function(){
+  console.log('listening on port 3000')
+});
